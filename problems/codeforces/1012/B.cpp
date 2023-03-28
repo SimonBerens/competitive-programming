4d392747b436964091baf8d276a74b19d473ac88
sberens <sberensnyc@gmail.com>
2023-03-28 14:28:26 -0700

    solve chemical table

---
+++ problems/codeforces/1012/B.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+template<typename K> using hset = gp_hash_table<K, null_type>;
+template<typename K, typename V> using hmap = gp_hash_table<K, V>;
+
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+
+template<typename T> void smin(T &x, T y) {
+    x = min(x, y);
+}
+
+template<typename T> void smax(T &x, T y) {
+    x = max(x, y);
+}
+
+template<typename T> T c2(T x) {
+    return x * (x - 1) / 2;
+}
+
+template<typename T> T sq(T x) {
+    return x * x;
+}
+
+#define FOR(i, a, b) for (ll i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i,0,a)
+#define ROF(i, a, b) for (ll i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i,0,a)
+
+using ll = long long;
+using ld = long double;
+
+template<typename T>
+using vv = vector<vector<T>>;
+
+using vi = vector<ll>;
+using ii = array<ll, 2>;
+using vii = vector<ii>;
+using vvi = vv<ll>;
+
+template<typename T>
+using minq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using maxq = priority_queue<T>;
+
+template<typename T>
+using oset = tree<T, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll M = 1000000007;
+const ll infll = M * M;
+
+void io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+void io(string name) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+    io();
+}
+
+vvi g;
+vi seen;
+
+void dfs(ll u) {
+    seen[u] = true;
+    for (ll v : g[u]) {
+        if (!seen[v]) {
+            dfs(v);
+        }
+    }
+}
+
+int main() {
+    io();
+    ll n, m, q;
+    cin >> n >> m >> q;
+    g.assign(n + m, {});
+    seen = vi(n + m);
+    F0R(i, q) {
+        ll r, c;
+        cin >> r >> c;
+        r--;
+        c--;
+        g[r].pb(c+n);
+        g[c+n].pb(r);
+    }
+    ll ci = 0;
+    F0R(i, n+m) {
+        if (!seen[i]) {
+            ci++;
+            dfs(i);
+        }
+    }
+    cout << ci - 1 << '\n';
+}
