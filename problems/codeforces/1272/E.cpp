436b6423e13651f04091d9d7be829fcab257e504
sberens <sberensnyc@gmail.com>
2023-04-03 14:07:42 -0700

    solve nearest opposite parity

---
+++ problems/codeforces/1272/E.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+template<typename K> using hset = gp_hash_table<K, null_type>;
+template<typename K, typename V> using hmap = gp_hash_table<K, V>;
+
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+
+template<typename T> void smin(T &x, T y) {
+    x = min(x, y);
+}
+
+template<typename T> void smax(T &x, T y) {
+    x = max(x, y);
+}
+
+template<typename T> T c2(T x) {
+    return x * (x - 1) / 2;
+}
+
+template<typename T> T sq(T x) {
+    return x * x;
+}
+
+#define FOR(i, a, b) for (ll i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i,0,a)
+#define ROF(i, a, b) for (ll i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i,0,a)
+
+using ll = long long;
+using ld = long double;
+
+template<typename T>
+using vv = vector<vector<T>>;
+
+using vi = vector<ll>;
+using ii = array<ll, 2>;
+using vii = vector<ii>;
+using vvi = vv<ll>;
+
+template<typename T>
+using minq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using maxq = priority_queue<T>;
+
+template<typename T>
+using oset = tree<T, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll M = 1000000007;
+const ll infll = M * M;
+
+void io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+void io(string name) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+    io();
+}
+
+vvi transpose(const vvi& graph) {
+    vvi t(graph.size());
+    F0R(u, graph.size())
+        for (int v : graph[u])
+            t[v].pb(u);
+    return t;
+}
+
+int main() {
+    io();
+    ll n;
+    cin >> n;
+    vi a(n);
+    F0R(i, n) cin >> a[i];
+    vvi g(n);
+    F0R(i, n) {
+        if (i + a[i] < n) {
+            g[i].pb(i + a[i]);
+        }
+        if (i - a[i] >= 0) {
+            g[i].pb(i - a[i]);
+        }
+    }
+    g = transpose(g);
+    vi d(n, -1);
+    F0R(p, 2) {
+        queue<ii> q;
+        vi seen(n);
+        F0R(i, n) {
+            if (a[i] % 2 == p) {
+                q.push({0, i});
+                seen[i] = true;
+            }
+        }
+        while (!q.empty()) {
+            auto [D, u] = q.front();
+            q.pop();
+            for (ll v: g[u]) {
+                if (a[v] % 2 == (p^1) && !seen[v]) {
+                    seen[v] = true;
+                    q.push({D+1, v});
+                    d[v] = D+1;
+                }
+            }
+        }
+    }
+    for (ll D : d) {
+        cout << D << ' ';
+    }
+    cout << '\n';
+
+}
