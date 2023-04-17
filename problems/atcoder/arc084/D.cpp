03f4e7d96c656da32a938bab77919cb296fa81d0
sberens <sberensnyc@gmail.com>
2023-04-16 21:58:04 -0700

    solve small multiple

---
+++ problems/atcoder/arc084/D.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+template<typename K> using hset = gp_hash_table<K, null_type>;
+template<typename K, typename V> using hmap = gp_hash_table<K, V>;
+
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+
+template<typename T> void smin(T &x, T y) {
+    x = min(x, y);
+}
+
+template<typename T> void smax(T &x, T y) {
+    x = max(x, y);
+}
+
+template<typename T> T c2(T x) {
+    return x * (x - 1) / 2;
+}
+
+template<typename T> T sq(T x) {
+    return x * x;
+}
+
+#define FOR(i, a, b) for (ll i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i,0,a)
+#define ROF(i, a, b) for (ll i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i,0,a)
+
+using ll = long long;
+using ld = long double;
+
+template<typename T>
+using vv = vector<vector<T>>;
+
+using vi = vector<ll>;
+using ii = array<ll, 2>;
+using vii = vector<ii>;
+using vvi = vv<ll>;
+
+template<typename T>
+using minq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using maxq = priority_queue<T>;
+
+template<typename T>
+using oset = tree<T, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll M = 1000000007;
+const ll infll = M * M;
+
+void io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+void io(string name) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+    io();
+}
+
+int main() {
+    io();
+    ll k;
+    cin >> k;
+    deque<ii> q; // i, c
+    q.push_back({1, 1});
+    vi seen(k, -1);
+    while (!q.empty()) {
+        auto [i, c] = q.front();
+        q.pop_front();
+        if (seen[i] != -1) continue;
+        seen[i] = c;
+        ll one = (i + 1) % k, ten = (i * 10) % k;
+        if (seen[one] == -1) {
+            q.push_back({one, c+1});
+        }
+        if (seen[ten] == -1) {
+            q.push_front({ten, c});
+        }
+    }
+    cout << seen[0] << '\n';
+}
