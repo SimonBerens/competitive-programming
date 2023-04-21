6165a88ef08ffe779c80a22b07157102e1ed6137
sberens <sberensnyc@gmail.com>
2023-04-21 15:00:52 -0700

    solve perfect groups

---
+++ problems/codeforces/980/D.cpp
@@ -0,0 +1,220 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+template<typename K> using hset = gp_hash_table<K, null_type>;
+template<typename K, typename V> using hmap = gp_hash_table<K, V>;
+
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+
+template<typename T>
+void smin(T &x, T y) {
+    x = min(x, y);
+}
+
+template<typename T>
+void smax(T &x, T y) {
+    x = max(x, y);
+}
+
+template<typename T>
+T c2(T x) {
+    return x * (x - 1) / 2;
+}
+
+template<typename T>
+T sq(T x) {
+    return x * x;
+}
+
+#define FOR(i, a, b) for (ll i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i,0,a)
+#define ROF(i, a, b) for (ll i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i,0,a)
+
+using ll = long long;
+using ld = long double;
+
+template<typename T>
+using vv = vector<vector<T>>;
+
+using vi = vector<ll>;
+using ii = array<ll, 2>;
+using vii = vector<ii>;
+using vvi = vv<ll>;
+
+template<typename T>
+using minq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using maxq = priority_queue<T>;
+
+template<typename T>
+using oset = tree<T, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll M = 1000000007;
+const ll infll = M * M;
+
+void io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+void io(string name) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+    io();
+}
+
+mt19937 randint(chrono::steady_clock::now().time_since_epoch().count());
+
+// returns a random integer over [a, b] inclusive
+inline int uniform_randint(const int a, const int b) {
+    return uniform_int_distribution<int>(a, b)(randint);
+}
+
+// returns a vector of length n, containing 1 if a number is prime, else 0.
+// runs in O(nloglogn) time.
+vector<bool> primesieve(int n) {
+    vector<bool> sieve(n, 1);
+    for (int i = 2; i < n; i++)
+        if (sieve[i])
+            for (int j = 2 * i; j < n; j += i)
+                sieve[j] = 0;
+    sieve[1] = 0;
+    return sieve;
+}
+
+// returns a sorted list of all primes less than or equal to n.
+// runs in O(nloglogn) time.
+vector<ll> primesupto(int n) {
+    vector<bool> sieve = primesieve(n + 1);
+    vector<ll> out;
+    for (int i = 2; i <= n; i++)
+        if (sieve[i]) out.push_back(i);
+    return out;
+}
+
+// checks if a number is prime in O(log^3(n)) time, randint comes from random.cpp.
+// works for n <= 10^18
+bool isprime(ll n) {
+    static vector<bool> sieve = primesieve(1000000);
+    if (n < sieve.size()) return sieve[n];
+    if (n % 2 == 0) return false;
+    ll d = n - 1, r = 0;
+    while (d % 2 == 0) r++, d >>= 1;
+    for (int k = 0; k < 30; k++) {
+        __int128_t x = 1, a = uniform_int_distribution<ll>(2, n - 2)(randint);
+        for (ll i = 1; i <= d; i <<= 1) {
+            if (d & i) x = (x * a) % n;
+            a = (a * a) % n;
+        }
+        bool f = 0;
+        if (x == 1 || x == n - 1) f = 1;
+        for (int i = 1; i < r; i++)
+            f = f || (x = (x * x) % n) == n - 1;
+        if (!f) return false;
+    }
+    return true;
+}
+
+// returns a sorted list of all prime factors of n in O(min(n^(1/2), n^(1/4)+log^3(n)+10^5)) time.
+// works for n <= 10^18
+vector<ll> primefactors(ll n) {
+    static vector<ll> small = primesupto(1000000);
+    if (isprime(n)) return {n};
+    vector<ll> out;
+    for (ll p: small) {
+        if (p * p > n) break;
+        while (n % p == 0)
+            n /= p, out.push_back(p);
+    }
+    if (n == 1 || isprime(n)) {
+        if (n != 1) out.push_back(n);
+        return out;
+    }
+    __int128_t x = 2, y = 2;
+    ll f = 0;
+    for (ll z = 1; 1; z <<= 1) {
+        y = x;
+        for (ll i = 0; i < z; i++)
+            if ((f = gcd(ll((x = (x * x + 1) % n) - y), n)) > 1) {
+                out.push_back(min(f, n / f)), out.push_back(max(f, n / f));
+                return out;
+            }
+    }
+    return out;
+}
+
+template<typename T>
+vector<pair<T, ll>> groupby(const vector<T> &a) {
+    vector<pair<T, ll>> groups;
+    for (const T &v: a) {
+        if (groups.empty() || groups.back().first != v) {
+            groups.eb(v, 0ll);
+        }
+        ++groups.back().second;
+    }
+    return groups;
+}
+
+template<typename T>
+pair<hmap<T, int>, hmap<int, T>> cord_compress(const vector<T> &cords, int offset = 0) {
+    hmap<T, int> compress;
+    hmap<int, T> decompress;
+    auto cpy = cords;
+    sort(all(cpy));
+    int lst = 0;
+    F0R(i, cpy.size()) {
+        if (compress.find(cpy[i]) == compress.end()) {
+            int x = lst++ + offset;
+            compress[cpy[i]] = x;
+            decompress[x] = cpy[i];
+        }
+    }
+    return {compress, decompress};
+}
+
+int main() {
+    io();
+    ll n;
+    cin >> n;
+    vi a(n);
+    F0R(i, n) cin >> a[i];
+    F0R(i, n) {
+        if (a[i] == 0) continue;
+        ll x = a[i] / abs(a[i]);
+        for (auto [p, c]: groupby(primefactors(abs(a[i])))) {
+            if (c % 2) x *= p;
+        }
+        a[i] = x;
+    }
+    auto [comp, decomp] = cord_compress(a, 1);
+    F0R(i, n) {
+        if (a[i]) a[i] = comp[a[i]];
+    }
+    vi res(n + 1);
+    F0R(i, n) {
+        vi seen(comp.size() + 2);
+        ll cnt = 0;
+        bool z = false;
+        FOR(j, i, n) {
+            if (a[j] == 0) z = true;
+            else if (!seen[a[j]]) {
+                seen[a[j]] = true;
+                cnt++;
+            }
+
+            res[max<ll>(z, cnt)]++;
+        }
+    }
+    FOR(i, 1, n + 1) {
+        cout << res[i] << ' ';
+    }
+    cout << '\n';
+}
