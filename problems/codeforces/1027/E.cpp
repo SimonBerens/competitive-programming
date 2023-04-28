ab395ad4eb72fea78cb583409c7af169f06f4bab
sberens <sberensnyc@gmail.com>
2023-04-28 12:24:26 -0700

    solve inverse coloring

---
+++ problems/codeforces/1027/E.cpp
@@ -0,0 +1,155 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+template<typename K> using hset = gp_hash_table<K, null_type>;
+template<typename K, typename V> using hmap = gp_hash_table<K, V>;
+
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+
+template<typename T> void smin(T &x, T y) {
+    x = min(x, y);
+}
+
+template<typename T> void smax(T &x, T y) {
+    x = max(x, y);
+}
+
+template<typename T> T c2(T x) {
+    return x * (x - 1) / 2;
+}
+
+template<typename T> T sq(T x) {
+    return x * x;
+}
+
+template<typename T> T ceildiv(T p, T q) {
+    return (p + q - 1) / q;
+}
+
+#define FOR(i, a, b) for (ll i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i,0,a)
+#define ROF(i, a, b) for (ll i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i,0,a)
+
+using ll = long long;
+using ld = long double;
+
+template<typename T>
+using vv = vector<vector<T>>;
+
+using vi = vector<ll>;
+using ii = array<ll, 2>;
+using vii = vector<ii>;
+using vvi = vv<ll>;
+
+template<typename T>
+using minq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using maxq = priority_queue<T>;
+
+template<typename T>
+using oset = tree<T, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll M = 998244353;
+const ll infll = M * M;
+
+void io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+void io(string name) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+    io();
+}
+
+template<ll M>
+struct modint {
+
+    static ll _reduce(ll n) {
+        constexpr static ll b = -1ull/M;
+        ll r = n-(ll)(__uint128_t(b)*n>>64)*M; return r >= M ? r-M : r;
+    }
+
+    static ll _pow(ll n, ll k) {
+        ll r = 1;
+        for (; k > 0; k >>= 1, n = _reduce(n*n))
+            if (k&1) r = _reduce(r*n);
+        return r;
+    }
+
+    ll v; modint(ll n = 0) : v(_reduce(n)) { v += (M&(0-(v<0))); }
+
+    friend string to_string(const modint n) { return to_string(n.v); }
+    friend istream& operator>>(istream& i, modint& n) { return i >> n.v; }
+    friend ostream& operator<<(ostream& o, const modint n) { return o << n.v; }
+    template<typename T> explicit operator T() { return T(v); }
+
+    friend bool operator==(const modint n, const modint m) { return n.v == m.v; }
+    friend bool operator!=(const modint n, const modint m) { return n.v != m.v; }
+    friend bool operator<(const modint n, const modint m) { return n.v < m.v; }
+    friend bool operator<=(const modint n, const modint m) { return n.v <= m.v; }
+    friend bool operator>(const modint n, const modint m) { return n.v > m.v; }
+    friend bool operator>=(const modint n, const modint m) { return n.v >= m.v; }
+
+    modint& operator+=(const modint n) { v += n.v; v -= (M&(0-(v>=M))); return *this; }
+    modint& operator-=(const modint n) { v -= n.v; v += (M&(0-(v<0))); return *this; }
+    modint& operator*=(const modint n) { v = _reduce(v*n.v); return *this; }
+    modint& operator/=(const modint n) { v = _reduce(v*_pow(n.v, M-2)); return *this; }
+    friend modint operator+(const modint n, const modint m) { return modint(n) += m; }
+    friend modint operator-(const modint n, const modint m) { return modint(n) -= m; }
+    friend modint operator*(const modint n, const modint m) { return modint(n) *= m; }
+    friend modint operator/(const modint n, const modint m) { return modint(n) /= m; }
+    modint& operator++() { return *this += 1; }
+    modint& operator--() { return *this -= 1; }
+    modint operator++(int) { modint t = *this; return *this += 1, t; }
+    modint operator--(int) { modint t = *this; return *this -= 1, t; }
+    modint operator+() { return *this; }
+    modint operator-() { return modint(0) -= *this; }
+
+    // O(logk) modular exponentiation
+    modint pow(const ll k) const {
+        return k < 0 ? _pow(v, M-1-(-k%(M-1))) : _pow(v, k);
+    }
+    modint inv() const { return _pow(v, M-2); }
+};
+
+using mi = modint<M>;
+using vmi = vector<mi>;
+
+int main() {
+    io();
+    ll n, k;
+    cin >> n >> k;
+    // dp[i] = number of configs of size i that don't have more than m in a row
+    // dp[i] = dp[i-1] + dp[i-2] + ... + dp[i-m] (dp[0] = 2)
+    vmi exact(n+1), at_most(n+1);
+    FOR(c, 1, n+1) {
+        vector<array<mi, 2>> dp(n+1); // 0 -> no c, 1 -> yes c
+        dp[0][0] = 2;
+        F0R(i, n) {
+            FOR(j, 1, c) {
+                if (i + j > n) break;
+                dp[i+j][0] += dp[i][0];
+                dp[i+j][1] += dp[i][1];
+            }
+            if (i + c <= n) dp[i+c][1] += dp[i][1] + dp[i][0];
+        }
+        at_most[c] = dp[n][0] + dp[n][1];
+        exact[c] = dp[n][1];
+    }
+    mi res;
+    FOR(c, 1, min(n, k)+1) {
+        ll r = min(n, ceildiv(k, c)-1);
+        res += exact[c] * at_most[r] / 2;
+    }
+    cout << res << '\n';
+}
