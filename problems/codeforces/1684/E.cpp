4bb896382778b021a6658b6f56c13ef949475d99
sberens <sberensnyc@gmail.com>
2023-05-30 00:52:03 -0700

    solve mex vs diff

---
+++ problems/codeforces/1684/E.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+template<typename K> using hset = gp_hash_table<K, null_type>;
+template<typename K, typename V> using hmap = gp_hash_table<K, V>;
+
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+
+template<typename T> void smin(T &x, T y) {
+    x = min(x, y);
+}
+
+template<typename T> void smax(T &x, T y) {
+    x = max(x, y);
+}
+
+template<typename T> T c2(T x) {
+    return x * (x - 1) / 2;
+}
+
+template<typename T> T sq(T x) {
+    return x * x;
+}
+
+template<typename T> T ceildiv(T p, T q) {
+    return (p + q - 1) / q;
+}
+
+#define FOR(i, a, b) for (ll i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i,0,a)
+#define ROF(i, a, b) for (ll i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i,0,a)
+
+using ll = long long;
+using ld = long double;
+
+template<typename T>
+using vv = vector<vector<T>>;
+
+using vi = vector<ll>;
+using ii = array<ll, 2>;
+using vii = vector<ii>;
+using vvi = vv<ll>;
+
+template<typename T>
+using minq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using maxq = priority_queue<T>;
+
+template<typename T>
+using oset = tree<T, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll M = 1000000007;
+const ll infll = M * M;
+
+void io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+void io(string name) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+    io();
+}
+
+int main() {
+    io();
+    int t;
+    cin >> t;
+    for (int test_no = 0; test_no < t; ++test_no) {
+        ll n, k;
+        cin >> n >> k;
+        vi a(n);
+        F0R(i, n) cin >> a[i];
+        map<ll, ll> cnt;
+        for(ll x : a) cnt[x]++;
+        vii v;
+        for (auto [x, c] : cnt) v.pb({x, c});
+        ll i = 0;
+        for (; i < v.size() && v[i][0] - i <= k; i++);
+        vi bad;
+        for (; i < v.size(); i++) {
+            bad.pb(v[i][1]);
+        }
+        sort(all(bad));
+        ll res = bad.size();
+        F0R (j, bad.size()) {
+            if (bad[j] <= k) {
+                k -= bad[j];
+                res--;
+            }
+        }
+        cout << res << '\n';
+    }
+}
